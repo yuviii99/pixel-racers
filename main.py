94a2a9022d0ee4fb0ee38992993dcff9b80a0501
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2024-05-15 18:38:40 +1000

    Handle collision with the track borders

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from utils import scale_image, rotate_image
 GRASS = scale_image(pygame.image.load("assets/grass.jpg"), 2.5)
 TRACK = scale_image(pygame.image.load("assets/track.png"), 0.9)
 TRACK_BORDER = scale_image(pygame.image.load("assets/track-border.png"), 0.9)
+TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
+
 FINISH = pygame.image.load("assets/finish.png")
+FINISH_POSITION = (130, 250)
 
 RED_CAR = scale_image(pygame.image.load("assets/red-car.png"), 0.5)
 GREEN_CAR = scale_image(pygame.image.load("assets/green-car.png"), 0.5)
@@ -51,6 +54,12 @@ class AbstractCar:
         self.x -= horizontal_vel
         self.y -= vertical_vel
         
+    def collide(self, mask, x=0, y=0):
+        car_mask = pygame.mask.from_surface(self.img)
+        offset = (int(self.x - x), int(self.y - y))
+        intersection_point = mask.overlap(car_mask, offset)
+        return intersection_point
+        
 
 class PlayerCar(AbstractCar):
     IMG = RED_CAR
@@ -58,6 +67,10 @@ class PlayerCar(AbstractCar):
     
     def reduce_speed(self):
         self.vel = max(self.vel - self.acceleration/2, 0) # So that vel doesn't get negative and car move backwards
+        self.move()
+    
+    def bounce(self):
+        self.vel = -self.vel
         self.move()
 
 def draw(WINDOW, images, player_car):
@@ -87,7 +100,7 @@ def move_player(player_car):
 
 run = True
 clock = pygame.time.Clock()
-images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
+images = [(GRASS, (0, 0)), (TRACK, (0, 0)), (FINISH, FINISH_POSITION), (TRACK_BORDER, (0, 0))]
 player_car = PlayerCar(4, 4)
 
 while run:
@@ -103,3 +116,5 @@ while run:
             break
     
     move_player(player_car)
+    if player_car.collide(TRACK_BORDER_MASK) != None:
+        player_car.bounce()
